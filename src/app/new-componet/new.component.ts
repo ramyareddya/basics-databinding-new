afcba674e4ec0bd47158bcd7c14ea6693bcb1657
sahithi <sahithicr@gmail.com>
2017-06-03 20:39:10 -0400

    THis is a new compinent

---
+++ src/app/new-componet/new.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './new.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+}
